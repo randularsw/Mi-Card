1bef84e5a5e569b04004cd5ad947e1d2748329ce
Randula Wickramasuriya <randularsw@gmail.com>
2020-04-18 17:30:56 +0530

    cards and effects

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,79 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal[800],
         body: SafeArea(
-            child: Column(children: <Widget>[
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: NetworkImage(
-                'https://lh3.googleusercontent.com/proxy/XqEMMueteZwNhW65uUJo7eekZyjMnVWL8zfZIR4diSvUL9lKvjoibpWHVrn-dpZDDH_JdPeZpuZP4VXl-sUU0_CJiZiOxynO0ssSqGHhWPkzmZ66Rlhf-r1aicyjB8Wn_Q'),
-          ),
-          Text(
-            'Reaper',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontFamily: 'Comfortaa',
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'FLUTTER DEVELOPER',
-            style: TextStyle(
-              color: Colors.teal[50],
-              fontSize: 15,
-              fontFamily: 'SourceSansPro',
-              letterSpacing: 2.5,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ])),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://lh3.googleusercontent.com/proxy/XqEMMueteZwNhW65uUJo7eekZyjMnVWL8zfZIR4diSvUL9lKvjoibpWHVrn-dpZDDH_JdPeZpuZP4VXl-sUU0_CJiZiOxynO0ssSqGHhWPkzmZ66Rlhf-r1aicyjB8Wn_Q'),
+              ),
+              Text(
+                'Reaper',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontFamily: 'Comfortaa',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal[50],
+                  fontSize: 15,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[50],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+94 123 456 789',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 15,
+                      fontFamily: 'SourceSansPro',
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'abcdef@mail.com',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontSize: 15,
+                      fontFamily: 'SourceSansPro',
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ])),
       ),
     );
   }
